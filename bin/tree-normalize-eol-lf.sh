448e123cc61976009fe65c1e8651ef26b1acda06
Garret Wilson <garret@globalmentor.com>
2016-08-28 20:36:01 -0700

    Fixed bug in EOL normalization shell script to support spaces in filenames.

--- bin/tree-normalize-eol-lf.sh
+++ bin/tree-normalize-eol-lf.sh
@@ -3,4 +3,4 @@
 #Copyright (c) 2015 GlobalMentor, Inc.
 #Recursively converts EOLs of common text types to Unix LF.
 set -e
-find . -type f -iregex ".*\(^2F^2FEN\|\.classpath\|\.css\|\.guisetheme\|\.htm\|\.html\|\.java\|\.js\|\.log\|\.lst\|\.md\|\.project\|\.php\|\.properties\|\.sql\|\.svg\|\.turf\|\.txt\|\.vcf\|\.xhtml\|\.xml\)" -not -path "./git/*" | while read file; do dos2unix $file; done
+find . -type f -iregex ".*\(^2F^2FEN\|\.classpath\|\.css\|\.guisetheme\|\.htm\|\.html\|\.java\|\.js\|\.log\|\.lst\|\.md\|\.project\|\.php\|\.properties\|\.sql\|\.svg\|\.turf\|\.txt\|\.vcf\|\.xhtml\|\.xml\)" -not -path "./git/*" | while read file; do dos2unix "$file"; done
