9cc489f566909af92a321ccab86eddc3239b7628
Garret Wilson <garret@globalmentor.com>
2017-03-26 16:36:34 -0700

    Updated Windows in-each-dir batch file arguments to match Linux version.

--- bin/in-each-dir.bat
+++ bin/in-each-dir.bat
@@ -3,14 +3,15 @@ REM In Each Directory
 REM Copyright (c) 2016 GlobalMentor, Inc.
 
 IF [%1]==[] GOTO usage
-IF [%2]==[] GOTO usage
+IF NOT [%2]==[--do] GOTO usage
+IF [%3]==[] GOTO usage
 
 ECHO Performing command inside all directories %1...
 FOR /D %%D in (%1) DO (
 	ECHO.
 	ECHO %%D...
 	pushd %%D
-	%2 %3 %4 %5 %6 %7 %8 %9
+	%3 %4 %5 %6 %7 %8 %9
 	popd
 )
 GOTO :eof
@@ -18,5 +19,5 @@ GOTO :eof
 :usage
 @ECHO In Each Directory
 @ECHO Executes a command inside each directory that matches some glob.
-@ECHO Usage: in-each-dir ^<dir-glob^> ^<command^> [^<parameter^>]...
+@ECHO Usage: in-each-dir ^<dir-glob^> --do ^<command^> [^<parameter^>]...
 EXIT /B 1
