eaf7f12fb43d8c03aa72e41674f2a35f8b4f7ca8
Garret Wilson <garret@globalmentor.com>
2018-12-17 07:01:12 -0800

    Added utility batch file for updating the Maven version(s) of a project.

---
+++ bin/mvn-set-ver.bat
@@ -0,0 +1,39 @@
+@ECHO OFF
+REM Maven Set Versions by Garret Wilson
+REM Copyright (c) 2018 GlobalMentor, Inc.
+
+SETLOCAL
+
+SET NEW_VERSION=%~1
+IF "%NEW_VERSION%" == "" (
+  GOTO usage
+)
+
+SET ALL=
+
+SHIFT
+:args
+SET PARAM=%~1
+SET ARG=%~2
+IF "%PARAM%" == "--all" (
+  SHIFT
+  SET ALL=-DprocessAllModules
+) ELSE IF "%PARAM%" == "" (
+  GOTO endargs
+) ELSE (
+  ECHO Unrecognized option %1. 1>&2
+  ECHO:
+  GOTO usage
+)
+GOTO args
+:endargs
+
+mvn versions:set -DnewVersion=%NEW_VERSION% -DgenerateBackupPoms=false %ALL%
+GOTO :eof
+
+:usage
+ECHO mvn-set-ver
+ECHO Usage: mvn-set-ver ^<new-version^> [--all]
+ECHO Updates the version number(s) in a Maven POM and all child POMS.
+ECHO If --all is specified, all submodules will be updated even if they are not children.
+EXIT /B 1
