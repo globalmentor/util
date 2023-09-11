ad912edd7500e28bc6050cbbb6ca914e4f62096c
Garret Wilson <garret@globalmentor.com>
2023-09-11 12:54:42 -0700

    Removed PowerShell version of Maven version retrieval script.
    The `.ps1` version was impeding Bash autocomplete for the `.sh` version.

--- bin/mvn-get-ver.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-# Maven Get Version by Garret Wilson
-# Copyright © 2022 GlobalMentor, Inc.
-# Requires `org.apache.maven.plugins:maven-help-plugin:3.1.0` or later.
-
-mvn help:evaluate -Dexpression="project.version" --quiet -DforceStdout
